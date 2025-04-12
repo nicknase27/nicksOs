d4be340e90c3ae00e87714191af2953a424f9873
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-04-12 22:50:51 +0200

    fix disko?

--- flake.nix
+++ flake.nix
@@ -4,16 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  };
-
-  disko = {
+    disko = {
     url = "github:nix-community/disko";
     inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
+  outputs = { self, disko, nixpkgs, ... }@inputs: {
     nixosConfigurations.default= nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
