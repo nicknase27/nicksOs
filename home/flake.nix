c71d7740563e0d5b7074875f3b785ccb0a35c33a
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-10-17 20:52:36 +0200

    fix home flake system var

--- home/flake.nix
+++ home/flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration for nicksOs";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -19,13 +18,10 @@
     home-manager,
     stylix,
     ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
+  }: {
     homeConfigurations = {
       "nick@Theseus" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./hosts/Theseus/home.nix
           ./hosts/common.nix
@@ -33,16 +29,18 @@
         ];
         extraSpecialArgs = {hostname = "Theseus";};
       };
+
       "nick@Hermes" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./hosts/Hermes/home.nix
           ./hosts/common.nix
         ];
         extraSpecialArgs = {hostname = "Hermes";};
       };
+
       "nick@Iris" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs.legacyPackages.aarch64-linux;
         modules = [
           ./hosts/Iris/home.nix
           ./hosts/common.nix
