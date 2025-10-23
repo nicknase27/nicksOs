a6e6a2d62003868738fb54892d47cdfcd5f40aaf
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-10-23 12:25:27 +0200

    transfer secrets to Hermes

--- system/secrets/secrets.nix
+++ system/secrets/secrets.nix
@@ -1,14 +1,15 @@
 let
   Theseus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwouafzGDRGeMJQbm9ME/1CSkXicdL7TthJjWkhyLYd nick@Theseus";
+  Hermes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6WDX5De9IY7cfl4XZg6mWLVGDQli5/F2DNecZvs7gk nick@Hermes";
   Iris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWmvZWyYxj3QTHYfNaLXHYjAU8HlwjAL/zH09FCTw8g nick@Iris";
 in {
-  "smb.age".publicKeys = [Theseus];
-  "vaultwarden-token.age".publicKeys = [Theseus];
-  "navidrome-env.age".publicKeys = [Theseus];
-  "wireguard-conf.age".publicKeys = [Theseus];
-  "cloudflare.age".publicKeys = [Theseus];
-  "cloudflare-cert.age".publicKeys = [Theseus];
-  "cloudflare-creds.age".publicKeys = [Theseus];
-  "pihole-token.age".publicKeys = [Theseus];
-  "homarr-token.age".publicKeys = [Theseus];
+  "smb.age".publicKeys = [Theseus Hermes];
+  "vaultwarden-token.age".publicKeys = [Hermes];
+  "navidrome-env.age".publicKeys = [Hermes];
+  "wireguard-conf.age".publicKeys = [Hermes];
+  "cloudflare.age".publicKeys = [Hermes];
+  "cloudflare-cert.age".publicKeys = [Hermes];
+  "cloudflare-creds.age".publicKeys = [Hermes];
+  "pihole-token.age".publicKeys = [Hermes];
+  "homarr-token.age".publicKeys = [Hermes];
 }
