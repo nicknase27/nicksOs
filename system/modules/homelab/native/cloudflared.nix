bc82190713eb68a8573bf0009afc58fffd840155
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-10-23 16:23:55 +0200

    cloudflare things

--- system/modules/homelab/native/cloudflared.nix
+++ system/modules/homelab/native/cloudflared.nix
@@ -9,17 +9,17 @@
       credentialsFile = "/var/lib/cloudflared/911ae7cf-2eaf-400b-a6c2-35f79b7ffea5.json";
       certificateFile = "/var/lib/cloudflared/cert.pem";
       default = "http_status:404";
-      ingress = {
-        "nv.nicknase27.com" = {
-          service = "http://localhost:4533";
-        };
-        "jf.nicknase27.com" = {
-          service = "http://localhost:8096";
-        };
-        "vt.nicknse27.com" = {
-          service = "http://localhost:8222";
-        };
-      };
+      #ingress = {
+      #  "nv.nicknase27.com" = {
+      #    service = "http://localhost:4533";
+      #  };
+      #  "jf.nicknase27.com" = {
+      #    service = "http://localhost:8096";
+      #  };
+      #  "vt.nicknse27.com" = {
+      #    service = "http://localhost:8222";
+      #  };
+      #};
     };
   };
 }
