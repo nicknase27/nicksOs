bd754f5ce7af35374e2e32fc489fcc7c45f03d3c
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-10-23 21:53:18 +0200

    keepalived things

--- system/modules/homelab/docker/pihole.nix
+++ system/modules/homelab/docker/pihole.nix
@@ -76,56 +76,78 @@
       enable = true;
       openFirewall = true;
       extraConfig = ''
-        global_defs {
-            router_id HOST1
+          global_defs {
+              router_id HOST1
+              enable_script_security
+          }
+
+          vrrp_script chk_pihole {
+            script "/etc/keepalived/check_pihole.sh"
+            interval 5
+            fall 2
+            rise 1
         }
 
-        # VRRP instance for IPv4
-        vrrp_instance VI_1_IPV4 {
-            state MASTER
-            interface eno1
-            virtual_router_id 51
-            priority 150
-            advert_int 1
+          # VRRP instance for IPv4
+          vrrp_instance VI_1_IPV4 {
+              state MASTER
+              interface eno1
+              virtual_router_id 51
+              priority 150
+              advert_int 1
 
-            unicast_src_ip 192.168.178.210
-            unicast_peer {
-                192.168.178.202
-            }
+              unicast_src_ip 192.168.178.210
+              unicast_peer {
+                  192.168.178.202
+              }
 
-            virtual_ipaddress {
-                192.168.178.250/24
-            }
+              virtual_ipaddress {
+                  192.168.178.250/24
+              }
 
-            authentication {
-                auth_type PASS
-                auth_pass AdfG4IJK
-            }
-        }
+              authentication {
+                  auth_type PASS
+                  auth_pass AdfG4IJK
+              }
 
-        # VRRP instance for IPv6
-        vrrp_instance VI_1_IPV6 {
-            state MASTER
-            interface eno1
-            virtual_router_id 52
-            priority 150
-            advert_int 1
+              track_script {
+                chk_pihole
+              }
+          }
 
-            unicast_src_ip fda0:be70:c013:0:5d95:9efc:6bcb:cff2
-            unicast_peer {
-                fda0:be70:c013:0:be24:11ff:fe84:ac69
-            }
+          # VRRP instance for IPv6
+          vrrp_instance VI_1_IPV6 {
+              state MASTER
+              interface eno1
+              virtual_router_id 52
+              priority 150
+              advert_int 1
 
-            virtual_ipaddress {
-                fda0:be70:c013::250/64
-            }
+              unicast_src_ip fda0:be70:c013:0:5d95:9efc:6bcb:cff2
+              unicast_peer {
+                  fda0:be70:c013:0:be24:11ff:fe84:ac69
+              }
 
-            #authentication {
-            #    auth_type PASS
-            #    auth_pass AdfG4IJK
-            #}
-        }
+              virtual_ipaddress {
+                  fda0:be70:c013::250/64
+              }
+
+              #authentication {
+              #    auth_type PASS
+              #    auth_pass AdfG4IJK
+              #}
+
+              track_script {
+                chk_pihole
+              }
+          }
       '';
     };
+  };
+
+  users.users.keepalived_script = {
+    isSystemUser = true;
+    description = "Unprivileged user for Keepalived health scripts";
+    shell = "/run/current-system/sw/bin/nologin";
   };
 }
