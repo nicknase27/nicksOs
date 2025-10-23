cc92794264128c4f5022bfe9f567ab12e6f02248
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-10-23 12:27:21 +0200

    a

--- system/hosts/Theseus/theseus-secrets.nix
+++ system/hosts/Theseus/theseus-secrets.nix
@@ -7,39 +7,6 @@
         path = "/etc/credentials.txt";
         mode = "777";
       };
-      #vaultwarden-token = {
-      #  file = ../../secrets/vaultwarden-token.age;
-      #  path = "/var/lib/vaultwarden/vaultwarden.env";
-      #};
-      navidrome-env = {
-        file = ../../secrets/navidrome-env.age;
-        path = "/var/lib/secrets/navidrome.env";
-      };
-      wireguard-conf = {
-        file = ../../secrets/wireguard-conf.age;
-        path = "data/.secret/wg.conf";
-      };
-      cloudflare = {
-        file = ../../secrets/cloudflare.age;
-      };
-      cloudflare-cert = {
-        file = ../../secrets/cloudflare-cert.age;
-        path = "/var/lib/cloudflared/cert.pem";
-      };
-      cloudflare-creds = {
-        file = ../../secrets/cloudflare-creds.age;
-        path = "/var/lib/cloudflared/911ae7cf-2eaf-400b-a6c2-35f79b7ffea5.json";
-      };
-      pihole-token = {
-        file = ../../secrets/pihole-token.age;
-        path = "/var/lib/secrets/pihole-token.env";
-        mode = "777";
-      };
-      homarr-token = {
-        file = ../../secrets/homarr-token.age;
-        path = "/var/lib/secrets/homarr-token.env";
-        mode = "777";
-      };
     };
     identityPaths = ["/etc/ssh/id_ed25519"];
   };
--- system/modules/homelab.nix
+++ system/modules/homelab.nix
@@ -1,16 +1,16 @@
 {...}: {
   imports = [
     # --- Native --- #
-    #./homelab/native/navidrome.nix
-    #./homelab/native/vaultwarden.nix
+    ./homelab/native/navidrome.nix
+    ./homelab/native/vaultwarden.nix
     #./homelab/native/cloudflared.nix
     #./homelab/native/audiobookshelf.nix
-    #./homelab/native/nixarr.nix
+    ./homelab/native/nixarr.nix
 
     # --- Docker --- #
-    #./homelab/docker/docker.nix
-    #./homelab/docker/pihole.nix
-    #./homelab/docker/nginxpm.nix
+    ./homelab/docker/docker.nix
+    ./homelab/docker/pihole.nix
+    ./homelab/docker/nginxpm.nix
     #./homelab/docker/homarr.nix
   ];
 }
