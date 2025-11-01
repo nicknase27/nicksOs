7eed9e45546e52f31127d58c4cd6895fb0c70933
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2025-11-01 15:20:02 +0000

    Use NixOS - Proxmox keepalived temporarily

--- system/modules/homelab/keepalived/master.nix
+++ system/modules/homelab/keepalived/master.nix
@@ -15,7 +15,7 @@
 
           unicastSrcIp = "192.168.178.210";
           unicastPeers = [
-            "192.168.178.215"
+            "192.168.178.202" ### CHANGE TO 215
           ];
 
           virtualIps = [
@@ -33,7 +33,7 @@
 
           unicastSrcIp = "fda0:be70:c013:0::210";
           unicastPeers = [
-            "fda0:be70:c013:0::215"
+            "fda0:be70:c013:0::202" ### CHANGE TO 215
           ];
 
           virtualIps = [
